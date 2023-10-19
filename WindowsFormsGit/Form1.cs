8c6ccee6be4d61e90cf503c8cd09f012d643bcc1
Alex-Ru-Tsar-of-all-Russia <aleks-aleks-2000-02@yandex.ru>
2023-10-19 23:38:42 +0700

    sV4

--- WindowsFormsGit/Form1.cs
+++ WindowsFormsGit/Form1.cs
@@ -8,8 +8,6 @@ namespace WindowsFormsGit
         {
             InitializeComponent();
             this.Text = "Git";
-            this.Text = "newGit";
-            int b = 5;
         }
     }
 }
